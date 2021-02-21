e21af4210afb226cae8b58c64a83321b89e3a966
shubhsmsahu <75929395+shubhsmsahu@users.noreply.github.com>
2021-02-21 18:19:58 +0530

    DigitalNewbutAddclear

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+export default function App() {
+  let time = new Date().toLocaleTimeString();
+
+  const [CurrTime, setTime] = useState(time);
+  const UpdateData = () => {
+    time = new Date().toLocaleTimeString();
+    setTime(time);
+  };
+  setInterval(UpdateData, 1000);
+  return (
+    <div id="main">
+      <h1 id="time">{CurrTime}</h1>
+    </div>
+  );
 }
 
 
