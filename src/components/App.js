f990b43560c5029f3977b06bd83d4b62f056cb68
shubhamsahu <sahu4816@gmail.com>
2021-02-24 13:56:59 +0530

    newcodeUpdate

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useEffect, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
+function App() {
+  let time = new Date().toLocaleTimeString();
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+  const [CurrTime, setTime] = useState(time);
+  const UpdateData = () => {
+    time = new Date().toLocaleTimeString();
+    setTime(time);
+  };
+  let set = setInterval(UpdateData, 1000);
+  useEffect(() => {
+    clearInterval(set);
+  });
+  return (
+    <div id="main" className="Clock">
+      <h1 id="time">{CurrTime}</h1>
+    </div>
+  );
 }
-
 
 export default App;
